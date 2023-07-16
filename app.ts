20ffbc49b398d27203a72d69f4e7b341c84612d0
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-16 17:36:30 +0300

    add context and example route

--- app.ts
+++ app.ts
@@ -1,9 +1,25 @@
-import Fastify from 'fastify';
-import AutoLoad from '@fastify/autoload';
-import Swagger from '@fastify/swagger';
-import SwaggerUI from '@fastify/swagger-ui';
+import Fastify from "fastify";
+import AutoLoad from "@fastify/autoload";
+import Swagger from "@fastify/swagger";
+import SwaggerUI from "@fastify/swagger-ui";
+import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
+import { createContext } from "./context/context";
+import { exampleRouter } from "./routes/router";
+import { router } from "./trpc";
 
-export function build () {
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = router({
+  example: exampleRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+export function build() {
   const server = Fastify();
 
   server.register(Swagger);
@@ -13,5 +29,10 @@ export function build () {
     dir: `${__dirname}/routes`,
   });
 
-  return server
+  server.register(fastifyTRPCPlugin, {
+    prefix: "/trpc",
+    trpcOptions: { router: appRouter, createContext },
+  });
+
+  return server;
 }
---
+++ routes/router.ts
@@ -0,0 +1,11 @@
+import { z } from "zod"
+
+import { publicProcedure, router } from "../trpc"
+
+export const exampleRouter = router({
+  hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
+    return {
+      greeting: `Hello ${input.text}`,
+    }
+  }),
+})
